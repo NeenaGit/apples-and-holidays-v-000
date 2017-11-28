4782370b37927e7375e1b9f79679c5782da5dbd3
Neena van de Water <nkuriakose@hotmail.com>
2017-11-28 19:59:37 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,6 +22,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
 
 end
 holiday_hash
+end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
