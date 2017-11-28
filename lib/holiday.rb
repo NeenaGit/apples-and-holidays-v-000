ecd706e5193a8c547181f75fed426a68c6bbc03f
Neena van de Water <nkuriakose@hotmail.com>
2017-11-28 21:19:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,6 +74,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
    holiday_hash.each do | holidays, supplies|
      if supplies == "BBQ"
-       return holidays
+       holidays
+     end
    end
 end
